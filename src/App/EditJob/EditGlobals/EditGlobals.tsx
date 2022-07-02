bd17d3846cf336386a4e278b31fd25fdb16fb05f
Lightling <contactlightling@gmail.com>
2022-07-02 17:53:49 -0400

    layout EditGlobals with test object

--- src/App/EditJob/EditGlobals/EditGlobals.tsx
+++ src/App/EditJob/EditGlobals/EditGlobals.tsx
@@ -1,12 +1,57 @@
+import { GlobalArchiveOption } from 'src/Types/Jobs/GlobalArchiveOption'
+import { CompressionMethodOptions } from 'src/Types/Jobs/CompressionMethod'
+import { CompressionLevelOptions } from 'src/Types/Jobs/CompressionLevel'
+import { EncryptionMethodOptions } from 'src/Types/Jobs/EncryptionMethod'
+import DropdownSelect from 'src/Shared/Input/DropdownSelect/DropdownSelect'
+import TextEntry from 'src/Shared/Input/TextEntry/TextEntry'
+
 import './EditGlobals.sass'
 
-function EditGlobals(props: any) {
+type EditGlobalsProps = {
+  globals: GlobalArchiveOption,
+}
+
+function EditGlobals(props: EditGlobalsProps) {
+  const {
+    globals,
+  } = props
+
   return (
     <div
       id="edit-globals"
       className="panel"
     >
       <h2>Edit Globals</h2>
+      <TextEntry
+        label={'Default Destination'}
+        value={globals.defaultDestination}
+        onChange={(val: string) => console.log(val)}
+      />
+      <DropdownSelect
+        options={CompressionLevelOptions}
+        label={'Default Compression Level'}
+        value={globals.defaultCompression.level}
+        onChange={(val: string) => console.log(val)}
+      />
+      <DropdownSelect
+        options={CompressionMethodOptions}
+        label={'Default Compression Method'}
+        value={globals.defaultCompression.level === 0 ? 'copy' : globals.defaultCompression.method}
+        onChange={(val: string) => console.log(val)}
+        disabled={globals.defaultCompression.level === 0 ? true : false}
+      />
+      <TextEntry
+        label={'Default Password'}
+        value={globals.defaultEncryption.password}
+        onChange={(val: string) => console.log(val)}
+      />
+      <DropdownSelect
+        options={EncryptionMethodOptions}
+        label={'Default Encryption Method'}
+        value={globals.defaultEncryption.password === '' ? 'none' : globals.defaultEncryption.method}
+        onChange={(val: string) => console.log(val)}
+        disabled={globals.defaultEncryption.password === '' ? true : false}
+      />
     </div>
   )
 }
--- src/App/EditJob/EditJob.tsx
+++ src/App/EditJob/EditJob.tsx
@@ -6,11 +6,25 @@ import EditGlobals from './EditGlobals/EditGlobals'
 import './EditJob.sass'
 
 function EditJob() {
+  const testGlobals: GlobalArchiveOption = {
+    defaultFormat: '7z',
+    defaultCompression: {
+      level: 0,
+      method: 'copy',
+    },
+    defaultDestination: '/',
+    defaultEncryption: {
+      password: '',
+      method: 'none',
+    }
+  }
+
   return (
     <div
       id="edit-job"
     >
       <EditGlobals
+        globals={testGlobals}
       />
       <EditItems
       />
